60872c3a65955de035dd76ff26b0140faa129504
WillV1243 <willv1243@gmail.com>
2020-07-30 15:26:28 +0100

    added scafolding modules

--- new-portfolio/src/app/app.module.ts
+++ new-portfolio/src/app/app.module.ts
@@ -1,14 +1,23 @@
+// angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// components
 import { AppComponent } from './app.component';
+
+// modules
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+/* --------------------------------------------------------------------------------- */
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
