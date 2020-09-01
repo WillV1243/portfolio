fd4a2f7382111f50d5d2b4e029b5a7db0f2de5bb
WillV1243 <willv1243@gmail.com>
2020-09-01 17:07:37 +0100

    split up code into new app structure successfully

--- portfolio-backend/src/app.js
+++ portfolio-backend/src/app.js
@@ -1,10 +1,10 @@
 // requirements
 const express = require('express');
-const cors = require('cors');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 // modules
-const transporter = require('./config/transporter');
+const router = require('./routes/routes');
 
 // declarations
 dotenv.config();
@@ -21,43 +21,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 // POST contact form
-app.post('/api/ContactForm', async (req, res) => {
-  console.log(req.body);
-  
-  try {
-    const mailOptions = {
-      from: req.body.email,
-      to: process.env.EMAIL,
-      subject: `[Mail sent from williamvandepeer.co.uk] ${req.body.name} from ${req.body.company}`,
-      html: req.body.message
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        res.status(500).send({
-          status: 500,
-          success: false,
-          message: 'Something went wrong. Try again later',
-          response: info.response
-        });
-      } else {
-        res.status(200).send({
-          status: 200,
-          success: true,
-          message: 'Thanks for sending a message!',
-          response: info.response
-        });
-      }
-    })
-  } catch (error) {
-    res.status(500).send({
-      status: 500,
-      success: false,
-      message: 'Something went wrong. Try again later',
-      response: info.response
-    });
-  }
-});
+app.use('/api', router);
 
 async function startServer() {
   // Server port
--- portfolio-backend/src/routes/routes.js
+++ portfolio-backend/src/routes/routes.js
@@ -1,48 +1,23 @@
 // requirements
-const express = require('express');
+const router = require('express').Router();
 
-// declarations
-const router = express.Router();
+// services
+const handleContactForm = require('../services/contact');
 /* --------------------------------------------------------------------------------- */
 
 // routes
 const postContactForm = async (req, res) => {
-  console.log(req.body);
   
-  try {
-    const mailOptions = {
-      from: req.body.email,
-      to: process.env.EMAIL,
-      subject: `[Mail sent from williamvandepeer.co.uk] ${req.body.name} from ${req.body.company}`,
-      html: req.body.message
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        res.status(500).send({
-          status: 500,
-          success: false,
-          message: 'Something went wrong. Try again later',
-          response: info.response
-        });
-      } else {
-        res.status(200).send({
-          status: 200,
-          success: true,
-          message: 'Thanks for sending a message!',
-          response: info.response
-        });
-      }
+  handleContactForm(req.body)
+    .then(handledRes => {
+      res.status(handledRes.status).send(handledRes);
     })
-  } catch (error) {
-    res.status(500).send({
-      status: 500,
-      success: false,
-      message: 'Something went wrong. Try again later'
-    });
-  }
+    .catch(err => {
+      console.error(err);
+    })
+
 };
 
-router.route('/api/ContactForm').post(postContactForm);
+router.post('/ContactForm', postContactForm);
 
 module.exports = router;
