aeabbd0899573ef4130cb02824f3b5219c075a18
WillV1243 <willv1243@gmail.com>
2020-09-03 09:54:46 +0100

    removed new portfolio

--- new-portfolio/README.md
+++
@@ -1,5 +0,0 @@
-# Portfolio
-
-Repo for new version of https://williamvandepeer.co.uk
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
--- new-portfolio/src/app/features/portfolio/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './landing/landing.component';
-export * from './project/project.component';
-export * from './skills/skills.component';
-export * from './about/about.component';
-export * from './contact/contact.component';
-export * from './technology-link/technology-link.component';
-export * from './contact-form/contact-form.component';
