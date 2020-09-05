d7577c353f2eb59ef69dafd94cc2d36349c77b6f
WillV1243 <willv1243@gmail.com>
2020-09-05 09:08:03 +0100

    completed style for contact form - still now server side error messages

--- portfolio-backend/src/services/stripHTML.js
+++ portfolio-backend/src/services/stripHTML.js
@@ -13,23 +13,22 @@ class StripRequestHTML {
   }
 
   stripHTML(string) {
-    if (string) {
-      let strippedString = string;
-  
-      strippedString = strippedString.replace(/<style([\s\S]*?)<\/style>/gi, '');
-      strippedString = strippedString.replace(/<script([\s\S]*?)<\/script>/gi, '');
-      strippedString = strippedString.replace(/<\/div>/ig, '\n');
-      strippedString = strippedString.replace(/<\/li>/ig, '\n');
-      strippedString = strippedString.replace(/<li>/ig, '  *  ');
-      strippedString = strippedString.replace(/<\/ul>/ig, '\n');
-      strippedString = strippedString.replace(/<\/p>/ig, '\n');
-      strippedString = strippedString.replace(/&nbsp;/ig, ' ');
-      strippedString = strippedString.replace(/<br\s*[\/]?>/gi, "\n");
-      strippedString = strippedString.replace(/<[^>]+>/ig, '');
-  
-      return strippedString;
-    }
-    return string;
+    if (!string) return;
+
+    let strippedString = string;
+
+    strippedString = strippedString.replace(/<style([\s\S]*?)<\/style>/gi, '');
+    strippedString = strippedString.replace(/<script([\s\S]*?)<\/script>/gi, '');
+    strippedString = strippedString.replace(/<\/div>/ig, '\n');
+    strippedString = strippedString.replace(/<\/li>/ig, '\n');
+    strippedString = strippedString.replace(/<li>/ig, '  *  ');
+    strippedString = strippedString.replace(/<\/ul>/ig, '\n');
+    strippedString = strippedString.replace(/<\/p>/ig, '\n');
+    strippedString = strippedString.replace(/&nbsp;/ig, ' ');
+    strippedString = strippedString.replace(/<br\s*[\/]?>/gi, "\n");
+    strippedString = strippedString.replace(/<[^>]+>/ig, '');
+
+    return strippedString;
   }
 
 }
